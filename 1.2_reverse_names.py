b192bd5ec7fe83939cb6046184febb9101b0aa14
MothishMC <73983645+MothishMC@users.noreply.github.com>
2021-07-31 23:53:49 +0530

    First

---
+++ 1.2_reverse_names.py
@@ -0,0 +1,3 @@
+first = input("Enter your first name : ")
+last = input("Enter your last name : ")
+print(f"{first[::-1]} {last[::-1]}")
---
+++ 1.3_sphere_volume.py
@@ -0,0 +1,5 @@
+import math
+
+diameter = 12
+volume = 4 / 3 * math.pi * (diameter / 2)**3
+print("The volume of the sphere is {:.4f} cm ^ 3".format(volume))
